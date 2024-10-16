c92f0d8767975f70b42ee213b3171e1ac808ffdf
zaid <zaid36@gmali.com>
2024-10-16 03:39:42 +0000

    CICD

---
+++ src/main/java/zaid/for_experiment/ForExperimentApplication.java
@@ -0,0 +1,13 @@
+package zaid.for_experiment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForExperimentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ForExperimentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/zaid/for_experiment/ForExperimentApplicationTests.java
@@ -0,0 +1,13 @@
+package zaid.for_experiment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForExperimentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
